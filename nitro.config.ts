8c4b5759b735869fa925fba4cb105c5dbe00913e
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-12-02 12:19:18 -0500

    chore: update config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,19 @@
 export default defineNitroConfig({
+  // See https://www.better-auth.com/docs/concepts/typescript
+  typescript: {
+    strict: true,
+    tsConfig: {
+      compilerOptions: {
+        // Declaration should be false. See https://www.better-auth.com/docs/concepts/typescript#declaration-files
+        declaration: false,
+        strict: true,
+      },
+    },
+  },
   srcDir: "server",
   compatibilityDate: "2024-11-28",
   timing: true,
+  imports: { dirs: ["server/utils"] },
   storage: {
     uploads: { driver: "fs", base: "./uploads" },
     // So you can access the email templates easily that are built by maizzle
